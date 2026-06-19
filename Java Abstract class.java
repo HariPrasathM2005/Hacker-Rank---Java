e65c10f261a6072375093565d026b813c801ee93
HariPrasathM2005 <mo.hariprasath@gmail.com>
2026-06-19 11:43:11 +0530

    Implement AdvancedArithmetic interface in MyCalculator

---
+++ Java Abstract class.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+interface AdvancedArithmetic
+{
+    int divisor_sum(int n);
+}
+class MyCalculator implements AdvancedArithmetic
+{
+    public int divisor_sum(int n)
+    {
+        int res=0,num=1;
+        while(num<=n)
+        {
+            if(n%num==0)
+            {
+                res=res+num;
+            }
+            num=num+1;
+        }
+        return res;
+    }
+}
+public class JavaInterface
+{
+
+    public static void main(String[] args) 
+    {
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+      	sc.close();
+        
+    }
+    static void ImplementedInterfaceNames(Object o){
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++){
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
+    }
+}
